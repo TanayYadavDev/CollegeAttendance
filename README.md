49980565fceec05266fbb124f1b32d314fe6a9ad
TanayYadavDev <ytanay710@gmail.com>
2026-07-22 19:41:17 +0530

    Initial Flutter project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# college_attendance
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/college_attendance/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.college_attendance
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
